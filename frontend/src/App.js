9b48ed0988be06d0ab7292c7efd3a7653151e498
TalYehuda <Talyehuda27@gmail.com>
2025-02-05 00:24:54 +0200

    minor fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Inbox from './pages/Inboxs';
+import Inbox from './pages/Inbox';
 
 
 const App = () => {
